3175ffb1c17b0898e076aad3e1c292e17ad077c8
04590876060 <04590876060@senacrs.edu.br>
2024-08-23 21:52:11 -0300

    atualizando atividade 2, colocando algumas mudanças necessárias 23/08/2024 21:52

--- atividades/atividade_002/src/screens/Home/styles.ts
+++ atividades/atividade_002/src/screens/Home/styles.ts
@@ -13,13 +13,51 @@ export const Title = styled.Text`
   border: 2px solid grey;
   border-radius: 20px;
   width: 90%;
-  margin-bottom: 180px;
+  margin-bottom: 2vh;
+  margin-top: 2vh;
+`;
+
+export const ColorBranco = styled.Text`
+  color: #cccccc;
+`;
+
+export const AdvancedDescription = styled.Text`
+  color: #9fa4aa;
+  font-size: 18px;
+  background-color: #1f1f1f;
+  padding: 10px;
+  border: 8px solid grey;
+  border-radius: 20px;
+  width: 90%;
+  margin-bottom: 4vh;
+  margin-top: 4vh;
+`;
+
+export const Description = styled.Text`
+  color: #899095;
+  font-size: 15px;
+  background-color: #3b3b3b;
+  padding: 10px;
+  border: 4px solid grey;
+  border-radius: 20px;
+  width: 90%;
+  margin-bottom: 2vh;
+  margin-top: 2vh;
 `;
 
 export const Container = styled.SafeAreaView`
   background-image: url(https://conteudo.imguol.com.br/c/noticias/3c/2020/07/03/homem-observa-estrelas-no-ceu-a-noite-1593802511360_v2_3x4.jpg);
   width: 400px;
-  height: 900px;
+  height: 2500px;
+  background-size: contain;
+  background-repeat: no-repeat;
+  align-items: center;
+  justify-content: flex-end;
+  background-color: black;
+`;
+
+export const ContainerTwo = styled.SafeAreaView`
+  background-image: url(https://th.bing.com/th/id/OIP.50Uvgvx89YswAjZgvjS-3AHaE8?rs=1&pid=ImgDetMain);
   background-size: contain;
   background-repeat: no-repeat;
   align-items: center;
