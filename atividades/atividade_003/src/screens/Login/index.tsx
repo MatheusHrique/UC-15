4d98d5019c681ffe6a8d4dd8aaf88abca286d9d9
04590876060 <04590876060@senacrs.edu.br>
2024-08-30 21:09:48 -0300

    adicionando atividade 3

---
+++ atividades/atividade_003/src/screens/Login/index.tsx
@@ -0,0 +1,12 @@
+import { SafeAreaView } from "react-native";
+import { Input } from "./style";
+
+const Index = () => {
+  return (
+    <SafeAreaView>
+      <Input />
+    </SafeAreaView>
+  );
+};
+
+export default Index;
---
+++ atividades/atividade_003/src/screens/Login/style.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components/native";
+
+export const Input = styled.TextInput`
+  color: #6200c4;
+`;
