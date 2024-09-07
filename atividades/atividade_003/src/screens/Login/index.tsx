bdc29c4ecac7f8be3afb74f512cdb87bdf6f7995
04590876060 <04590876060@senacrs.edu.br>
2024-09-06 21:49:31 -0300

    adicionado async storage na atividade_003

--- atividades/atividade_003/src/screens/Login/index.tsx
+++ atividades/atividade_003/src/screens/Login/index.tsx
@@ -8,18 +8,48 @@ import {
 } from "react-native";
 import { InputName, Container } from "./style";
 import { useEffect, useState } from "react";
-import { Sansita_400Regular, useFonts } from "@expo-google-fonts/sansita";
+import { Sansita_400Regular, useFonts } from "@expo-google-fonts/sansita"; // Necessário importar manualmente a fonte para utilização da propria; Autocomplete ajuda
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+interface IStorage {
+  // (I)Storage significa inferface; Está interface está servindo para atribuir value em storeData()
+  username: string;
+  senha: string;
+}
 
 const Index = () => {
-  const [username, setUsername] = useState<string>("");
-  const [senha, setSenha] = useState<string>("");
-  const [teste, setTeste] = useState<string>(
+  const [username, setUsername] = useState<string>(""); // Nome do usuario
+  const [senha, setSenha] = useState<string>(""); // Senha do usuario
+  const [mensagem, setMensagem] = useState<string>(
     "Necessita adicionar os dados acima."
-  );
+  ); //
 
   useFonts({
+    // Necessário useFonts para o funcionamento da fonte
     Sansita_400Regular,
   });
+
+  const storeData = async (value: IStorage) => {
+    const jsonValue = JSON.stringify(value); // Transforma o objeto em uma string
+    if (Platform.OS !== "web") {
+      try {
+        await AsyncStorage.setItem("@atividade_003-userInfo", jsonValue); //nomedoprojeto-identificação; string a ser salvo; salva no storage.
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      window.localStorage.setItem("@atividade_003-userInfo", jsonValue); //nomedoprojeto-identificação; string a ser salvo; salva no storage.
+    }
+  };
+
+  const getData = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem("@atividade_003-userInfo");
+      return jsonValue != null ? JSON.parse(jsonValue) : null;
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   useEffect(() => {
     if (Platform.OS !== "web") {
@@ -47,31 +77,52 @@ const Index = () => {
 
   const updateTeste = () => {
     if (username.length == 0) {
-      setTeste("Necessita adicionar o nome.");
+      setMensagem("Necessita adicionar o nome.");
       if (Platform.OS == "web") {
         window.alert(`Necessita adicionar o nome.`);
       } else {
         Alert.alert("Aviso", "Preencha o nome!", [{ text: "Ok" }]);
       }
     } else if (senha.length == 0) {
-      setTeste("Necessita adicionar a senha.");
+      setMensagem("Necessita adicionar a senha.");
       if (Platform.OS == "web") {
         window.alert(`Necessita adicionar a senha.`);
       } else {
         Alert.alert("Aviso", "Preencha a senha!", [{ text: "Ok" }]);
       }
     } else {
-      setTeste("Tudo certo!");
+      setMensagem("Tudo certo!");
       showLogedAlert();
     }
   };
 
   const showLogedAlert = () => {
+    getData().then((storage) => {
+      // explicação do .then: joga no edge copilot: "explique o .then()", é mais fácil de entender deste jeito
+      // Está servindo para detectar se for o mesmo usuário que está logando para enviar uma alert extra
+      // Está declarando 'storage' como o objeto que vier de 'getData()'
+      if (storage) {
+        if (storage.username == username) {
+          // '&& storage.senha == senha'
+          if (Platform.OS !== "web") {
+            Alert.alert("Aviso", `Opa! Bem vindo de volta amigo!`, [
+              { text: "Obrigado!" },
+            ]);
+          } else {
+            window.alert("Opa! Bem vindo de volta amigo!");
+          }
+        }
+      }
+    });
+    storeData({ username: username, senha: senha }); // Adicione suas variaveis strings/int/seila o que aqui para salvar.
+
     if (Platform.OS !== "web") {
+      // Verifica se a plataforma não é web
       Alert.alert("Login", `Nome: ${username}`, [
         { text: "Tudo certo", onPress: () => console.log("Logado!") },
       ]);
     } else {
+      // Se for web
       window.alert(`Nome: ${username}`);
     }
   };
@@ -101,7 +152,7 @@ const Index = () => {
           fontFamily: "Sansita_400Regular",
         }}
       >
-        {teste}
+        {mensagem}
       </Text>
       <TouchableOpacity onPress={updateTeste} style={styles.buttonLogin}>
         <Text style={{ fontFamily: "Sansita_400Regular" }}>Entrar</Text>
@@ -125,6 +176,18 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     backgroundColor: "#99a1a1",
     fontWeight: "bold",
-    fontFamily: "Sansita_400Regular",
+    fontFamily: "Sansita_400Regular", // Setando fonte aqui
   },
 });
+
+// *imports:
+// npm install --force
+// npm install styled-components
+// npx expo install react-native-web react-dom @expo/metro-runtime
+// npm install @react-native-async-storage/async-storage
+// npx expo install expo-font
+// npx expo install expo-font @expo-google-fonts/sansita
+
+// *exec:
+// npx expo --tunnel
+// npx expo start --web
